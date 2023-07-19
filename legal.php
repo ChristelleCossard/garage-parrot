231f95a6a0dce7ce8ffe2b7bf5aca0d58a04e7f3
Christelle Cossard <christelle.cossard10@gmail.com>
2023-07-19 22:32:45 +0200

    ajout dans menu

--- legal.php
+++ legal.php
@@ -1,6 +1,8 @@
-<?php
-  require_once('templates/header.php');
-  ?>
+<?php 
+require_once __DIR__ . "/lib/config.php";
+require_once __DIR__ . "/lib/menu.php"; 
+require_once __DIR__ . "/templates/header.php"; 
+?>
      
   
     <!-- Content -->
@@ -231,6 +233,4 @@
             
     </section>
    
-    <?php
-require_once('templates/footer.php');
-?> 
+    <?php require_once __DIR__ . "/templates/footer.php"; ?>
--- lib/menu.php
+++ lib/menu.php
@@ -5,4 +5,5 @@ $mainMenu = [
     "actualites.php" => ["menu_title" => "Actualités", "head_title" => "Actualités tech et dev", "meta_description" => "Découvrez toutes nos actualités."],
     "a_propos.php" => ["menu_title" => "A propos", "head_title" => "A propos de TechTrendz", "meta_description" => "L'histoire du site TechTrendz"],
     "login.php" => ["menu_title" => "Connexion", "head_title" => "Connexion TechTrendz", "meta_description" => "Connexion TechTrendz", "exclude" => true],
+    "legal.php" => ["menu_title" => "Mentions legales", "head_title" => "Connexion TechTrendz", "meta_description" => "Connexion TechTrendz", "exclude" => true],
 ];
