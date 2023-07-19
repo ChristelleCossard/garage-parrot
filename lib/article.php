2b32a0befa4b16d8776d99a6a63276ac42d8811c
Christelle Cossard <christelle.cossard10@gmail.com>
2023-07-19 21:14:23 +0200

    chgt structure projet

---
+++ lib/article.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+
+Page 1:
+LIMIT 0, 10
+Page 2:
+LIMIT 10, 10
+Page 3:
+LIMIT 20, 10
+Page 4:
+LIMIT 30, 10
+
+param : page et limit
+offest = (page - 1) * limit
+Page 3
+30     = (3 -1) * 10
+
+*/
+
+
+function getArticles(PDO $pdo, int $limit = null, int $page = null ):array
+{
+    $sql = "SELECT * FROM articles ORDER BY id DESC";
+    if ($limit && !$page) {
+        $sql .= " LIMIT :limit";
+    }
+    if ($page) {
+        $sql .= " LIMIT :offset, :limit";
+    }
+
+    $query = $pdo->prepare($sql);
+
+    if ($limit) {
+        $query->bindValue(":limit", $limit, PDO::PARAM_INT);
+    }
+    if ($page) {
+        $offset = ($page -1) * $limit;
+        $query->bindValue(":offset", $offset, PDO::PARAM_INT);
+    }
+
+    $query->execute();
+    $articles = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    return $articles;
+}
+
+
+function getArticleById(PDO $pdo, int $id):array|bool
+{
+    $sql = "SELECT * FROM articles WHERE id = :id";
+
+    $query = $pdo->prepare($sql);
+    
+    $query->bindValue(":id", $id, PDO::PARAM_INT);
+
+
+    $query->execute();
+    $article = $query->fetch(PDO::FETCH_ASSOC);
+
+    return $article;
+}
+
+function getArticleImage(string|null $image):string
+{
+    if ($image === null) {
+        return _ASSETS_IMAGES_FOLDER_."default-article.jpg";
+    } else {
+        return _ARTICLES_IMAGES_FOLDER_.htmlentities($image);
+    }
+}
--- templates/header.php
+++ templates/header.php
@@ -1,38 +1,54 @@
+<?php
+require_once __DIR__ . "/../lib/session.php";
+    $currentPage = basename($_SERVER["SCRIPT_NAME"]);
+
+?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/override-bootstrap.css">
+    <meta name="description" content="<?=$mainMenu[$currentPage]["meta_description"] ?>">
+    <title><?=$mainMenu[$currentPage]["head_title"] ?></title>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
-    <title>Bookeo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style1.css">
 </head>
 
 <body>
-
     <div class="container">
         <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-            <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-                <img width="90" src="assets/images/logo-bookeo.jpg" alt="Logo Bookeo">
-            </a>
+            <div class="col-md-3 mb-2 mb-md-0">
+                <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+                    <img src="assets/images/logo-tech-trendz.png" alt="logo TechTrendz" width="150">
+                </a>
+            </div>
 
-            <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                <li><a href="#" class="nav-link px-2 link-secondary">Home</a></li>
-                <li><a href="#" class="nav-link px-2 link-dark">Features</a></li>
-                <li><a href="#" class="nav-link px-2 link-dark">Pricing</a></li>
-                <li><a href="#" class="nav-link px-2 link-dark">FAQs</a></li>
-                <li><a href="#" class="nav-link px-2 link-dark">About</a></li>
+            <ul class="nav nav-pills col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+                <?php foreach ($mainMenu as $key => $menuItem) { 
+                    if (!array_key_exists("exclude", $menuItem)) {
+                    ?>
+                    <li class="nav-item"><a href="<?=$key; ?>" class="nav-link px-2 <?php 
+                        if ($key === $currentPage) { echo "active"; }
+                        //echo ($key === $currentPage) ? "active" : "";
+                        
+                        ?>"><?=$menuItem["menu_title"]; ?></a></li>
+                <?php } 
+                    }
+                ?>
             </ul>
 
             <div class="col-md-3 text-end">
-                <button type="button" class="btn btn-outline-primary me-2">Login</button>
-                <button type="button" class="btn btn-primary">Sign-up</button>
+                <?php if (isset($_SESSION["user"])) { ?>
+                    <a href="logout.php" class="btn btn-primary">Déconnexion</a>
+                <?php } else { ?>
+                    <a href="login.php" class="btn btn-outline-primary me-2">Connexion</a>
+                <?php } ?>
+                
             </div>
         </header>
+
 
         <main>
