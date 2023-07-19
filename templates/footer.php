abc5c08f83e53f4c00cd88640c6ef7cfb117d475
Christelle Cossard <christelle.cossard10@gmail.com>
2023-07-19 21:59:31 +0200

    footer perso

--- templates/footer.php
+++ templates/footer.php
@@ -1,15 +1,48 @@
 </main>
 
-<footer class="py-3 my-4">
-    <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Home</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Features</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Pricing</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQs</a></li>
-        <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">About</a></li>
-    </ul>
-    <p class="text-center text-body-secondary">© 2023 Company, Inc</p>
+<hr>
+<footer>
+        <div class="container-fluid">
+        <div class="d-flex justify-content-center">
+                    <div class="col-2">
+               <h2 align="center"> Nos horaires d'ouvertures:</h2>
+               <br>
+               <h3>Lundi:Fermé</h3>
+               <h3>Mardi:</h3><p>12:00 - 14:00</p><p>19:00 - 22:00</p>
+               <h3>Mercredi:</h3><p>12:00 - 14:00</p><p>19:00 - 22:00</p>
+               <h3>Jeudi:</h3><p>12:00 - 14:00</p><p>19:00 - 22:00</p>
+               <h3>Vendredi:</h3><p>12:00 - 14:00</p><p>19:00 - 22:00</p>
+              <h3>Samedi:</h3><p>12:00 - 14:00</p><p>19:00 - 22:00</p>
+               <h3>Dimanche:</h3><p>12:00 - 14:00</p><p>19:00 - 22:00</p>
+               <hr> 
+            </div></div>
+           
+            <div class="row footer">
+                <div class="d-flex justify-content-center">
+                    <div class="col-2">
+                        <ul class="list-unstyled text-center">
+                           
+                            <li><a class="nav-link" href="mailto:christelle.cossard10@gmail.com<">christelle.cossard10@gmail.com</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-6 text-center copyright">
+                        <br>
+                        © 2023 
+		  <a class="nav-link" href="http://cocoweb10.fr/" target="_blank"> Christelle Cossard.</a>
+                    </div>
+                    <div class="col-2">
+                        <ul class="list-unstyled text-center">
+                            <li><a class="nav-link" href="legal.php">Mentions légales</a></li>
+                        <!--   -->
+                
+                      </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
 </footer>
+  </div>
 
 </div>
 
