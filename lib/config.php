1118ca11fb4ca8fc9fe6efc4a2acd088f64897fc
Christelle Cossard <christelle.cossard10@gmail.com>
2023-07-27 22:20:10 +0200

    scripts bdd garage

--- lib/config.php
+++ lib/config.php
@@ -1,9 +1,12 @@
 <?php
 
 define("_DOMAIN_", ".techtrendzstudi.local");
-define("_DB_SERVER_", "mysql-cocoweb10.alwaysdata.net");
-define("_DB_NAME_", "cocoweb10_studitech");
-define("_DB_USER_", "cocoweb10");
+//define("_DB_SERVER_", "mysql-cocoweb10.alwaysdata.net");
+//define("_DB_NAME_", "cocoweb10_studitech");
+//define("_DB_USER_", "cocoweb10");
+define("_DB_SERVER_", "mysql-cocodev.alwaysdata.net");
+define("_DB_NAME_", "cocodev_garage");
+define("_DB_USER_", "cocodev");
 define("_DB_PASSWORD_", "Vicky.10");
 define("_ASSETS_IMAGES_FOLDER_", "/assets/images/");
 define("_ARTICLES_IMAGES_FOLDER_", "/uploads/articles/");
