5c4f5d7d339d7776c1b1fb2dc0310a2ab38925a0
Christelle Cossard <christelle.cossard10@gmail.com>
2023-11-21 21:15:26 +0100

    modif lien

--- admin/templates/header.php
+++ admin/templates/header.php
@@ -27,7 +27,7 @@ $adminMenu = [
     <div class="container d-flex">
 
         <div class="d-flex flex-column flex-shrink-0 p-3 text-bg-dark" style="width: 280px;">
-            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+            <a href="https://cocodev.alwaysdata.net/garage-parrot/index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
                 <span class="fs-4">Home</span>
             </a>
             <hr>
